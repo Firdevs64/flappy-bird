61042fce0dad8d72201a830b676b7a5247b5757f
Firdevs Köse <230541057@firat.edu.tr>
2024-11-06 22:43:20 +0300

    flappy bird
    Bu flappy bird oyununu C# kullanarak yaptım.

---
+++ Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace flappy_bird
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.flappyBird = new System.Windows.Forms.PictureBox();
+            this.Zemin = new System.Windows.Forms.PictureBox();
+            this.BoruUst = new System.Windows.Forms.PictureBox();
+            this.BoruAlt = new System.Windows.Forms.PictureBox();
+            this.scoreText = new System.Windows.Forms.Label();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.flappyBird)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Zemin)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BoruUst)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BoruAlt)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // flappyBird
+            // 
+            this.flappyBird.Image = ((System.Drawing.Image)(resources.GetObject("flappyBird.Image")));
+            this.flappyBird.Location = new System.Drawing.Point(29, 148);
+            this.flappyBird.Name = "flappyBird";
+            this.flappyBird.Size = new System.Drawing.Size(69, 67);
+            this.flappyBird.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.flappyBird.TabIndex = 0;
+            this.flappyBird.TabStop = false;
+            // 
+            // Zemin
+            // 
+            this.Zemin.Image = ((System.Drawing.Image)(resources.GetObject("Zemin.Image")));
+            this.Zemin.Location = new System.Drawing.Point(-9, 375);
+            this.Zemin.Name = "Zemin";
+            this.Zemin.Size = new System.Drawing.Size(570, 106);
+            this.Zemin.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Zemin.TabIndex = 0;
+            this.Zemin.TabStop = false;
+            // 
+            // BoruUst
+            // 
+            this.BoruUst.Image = ((System.Drawing.Image)(resources.GetObject("BoruUst.Image")));
+            this.BoruUst.Location = new System.Drawing.Point(395, 1);
+            this.BoruUst.Name = "BoruUst";
+            this.BoruUst.Size = new System.Drawing.Size(97, 116);
+            this.BoruUst.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.BoruUst.TabIndex = 1;
+            this.BoruUst.TabStop = false;
+            // 
+            // BoruAlt
+            // 
+            this.BoruAlt.Image = ((System.Drawing.Image)(resources.GetObject("BoruAlt.Image")));
+            this.BoruAlt.Location = new System.Drawing.Point(280, 271);
+            this.BoruAlt.Name = "BoruAlt";
+            this.BoruAlt.Size = new System.Drawing.Size(98, 122);
+            this.BoruAlt.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.BoruAlt.TabIndex = 2;
+            this.BoruAlt.TabStop = false;
+            // 
+            // scoreText
+            // 
+            this.scoreText.AutoSize = true;
+            this.scoreText.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.scoreText.Location = new System.Drawing.Point(12, 484);
+            this.scoreText.Name = "scoreText";
+            this.scoreText.Size = new System.Drawing.Size(123, 31);
+            this.scoreText.TabIndex = 3;
+            this.scoreText.Text = "Score: 0";
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Enabled = true;
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.gameTimerEvent);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PowderBlue;
+            this.ClientSize = new System.Drawing.Size(559, 519);
+            this.Controls.Add(this.flappyBird);
+            this.Controls.Add(this.scoreText);
+            this.Controls.Add(this.BoruAlt);
+            this.Controls.Add(this.BoruUst);
+            this.Controls.Add(this.Zemin);
+            this.Name = "Form1";
+            this.Text = "Flappy Bird Game";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.gamekeyisdown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.gamekeyisup);
+            ((System.ComponentModel.ISupportInitialize)(this.flappyBird)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Zemin)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BoruUst)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BoruAlt)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox flappyBird;
+        private System.Windows.Forms.PictureBox Zemin;
+        private System.Windows.Forms.PictureBox BoruUst;
+        private System.Windows.Forms.PictureBox BoruAlt;
+        private System.Windows.Forms.Label scoreText;
+        private System.Windows.Forms.Timer gameTimer;
+    }
+}
+
